ae88df08c9c424804e53f8c81dad62afef3773e2
diwasx <diwasx@gmail.com>
2019-10-21 12:06:10 +0545

    basic system complete

--- main/views.py
+++ main/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from subprocess import run, PIPE
 import sys
 from script import getData
 
 # Create your views here.
 def index(request):
-    raw_data = getData()
+    raw_data_common, raw_data_all = getData()
     common_symptom = []
-    for i in raw_data:
+    all_symptom = []
+    for i in raw_data_common:
         tmp = i.split('_', 1)[-1]
         common_symptom.append(tmp)
+
+    for i in raw_data_all:
+        tmp = i.split('_', 1)[-1]
+        all_symptom.append(tmp)
+
     # print(common_symptom)
-    # print(raw_data)
-    data = list(zip(raw_data, common_symptom))
-    # print(data)
+    # print(raw_data_all)
+    data = list(zip(raw_data_common, common_symptom))
+    data1 = list(zip(raw_data_all, all_symptom))
+    # print(data1)
+    # print(symptom_all)
 
     if request.method =='POST' and 'train_data' in request.POST:
         sys.path.append('..')
@@ -25,9 +33,25 @@ def index(request):
         # print("Predicting Disease")
         checked_list = request.POST.getlist('checks[]')
         # print(checked_list)
+        symptoms_set = request.POST.get("symptoms_field")
+        # print(symptoms_set)
+        symptoms_list = symptoms_set.split(",")
+        # print(symptoms_list)
+        merged_list = symptoms_list + checked_list
+        merged_list = list(set(merged_list))
+        print("\nInput are : \n",merged_list)
 
         sys.path.append('..')
         from script import predict
-        predict(checked_list)
+        # values =  predict(checked_list)
+        # values =  predict(symptoms_list)
 
-    return render(request, "home.html", {"data": data})
+        if all('' == s or s.isspace() for s in merged_list):
+            print("\nInput is empty:\n")
+            return render(request, "home.html", {"data": data, "data1": data1})
+
+        values =  predict(merged_list)
+        print("\nOutput are:\n",values,"\n")
+        return render(request, "home.html", {"data": data, "data1": data1, "predicted_diseases": values})
+
+    return render(request, "home.html", {"data": data, "data1": data1})
--- script.py
+++ script.py
@@ -4,6 +4,7 @@ import pandas as pd
 import xlrd
 from neural_network import NeuralNetwork
 import numpy as np
+from heapq import nlargest
 
 # Initialization
 nn = NeuralNetwork(402, 50, 134)
@@ -32,43 +33,44 @@ def training():
 
     #-- Prepare Input and target vector and feed it to NN for training --#
     # All inputs and target except last one
-    tmpInput = df.Disease.to_list()
-    tmpInd = 0
-    for i in tmpInput:
-        if( i!= "\xa0"):
-            inputVec = np.zeros(shape=(402,1))
-            targetVec = np.zeros(shape=(134,1))
-            ind = tmpInput.index(i)
-            if (ind != 0):
-                inputList = symptom_list[tmpInd:ind]
-                # print (inputList)
-                for k in inputList:
-                    vecInd = symptom_list_filter.index(k)
-                    inputVec[vecInd] += 1
-                # print(inputVec,"\n")
-                # print(disease_list.index(i))
-                targetVec[disease_list.index(i)-1] = 1
+    for loop in range(70):
+        tmpInput = df.Disease.to_list()
+        tmpInd = 0
+        for i in tmpInput:
+            if( i!= "\xa0"):
+                inputVec = np.zeros(shape=(402,1))
+                targetVec = np.zeros(shape=(134,1))
+                ind = tmpInput.index(i)
+                if (ind != 0):
+                    inputList = symptom_list[tmpInd:ind]
+                    # print (inputList)
+                    for k in inputList:
+                        vecInd = symptom_list_filter.index(k)
+                        inputVec[vecInd] += 1
+                    # print(inputVec,"\n")
+                    # print(disease_list.index(i))
+                    targetVec[disease_list.index(i)-1] = 1
 
-                # print("INPUT IS \n",inputVec)
-                # print("TARGET IS \n",targetVec)
-                nn.trainSVLearing(inputVec,targetVec,learningRate)
-                tmpInd = ind
+                    # print("INPUT IS \n",inputVec)
+                    # print("TARGET IS \n",targetVec)
+                    nn.trainSVLearing(inputVec,targetVec,learningRate)
+                    tmpInd = ind
 
-    # Last Inputs and Target
-    inputVec = np.zeros(shape=(402,1))
-    targetVec = np.zeros(shape=(134,1))
-    j = len(symptom_list)
-    inputList = symptom_list[tmpInd:j]
-    # print(inputList)
-    for k in inputList:
-        # print(k)
-        vecInd = symptom_list_filter.index(k)
-        # print(vecInd)
-        inputVec[vecInd] += 1
-    # print("INPUT IS \n",inputVec)
-    targetVec[-1:] = 1
-    # print("TARGET IS \n",targetVec)
-    nn.trainSVLearing(inputVec,targetVec,learningRate)
+        # Last Inputs and Target
+        inputVec = np.zeros(shape=(402,1))
+        targetVec = np.zeros(shape=(134,1))
+        j = len(symptom_list)
+        inputList = symptom_list[tmpInd:j]
+        # print(inputList)
+        for k in inputList:
+            # print(k)
+            vecInd = symptom_list_filter.index(k)
+            # print(vecInd)
+            inputVec[vecInd] += 1
+        # print("INPUT IS \n",inputVec)
+        targetVec[-1:] = 1
+        # print("TARGET IS \n",targetVec)
+        nn.trainSVLearing(inputVec,targetVec,learningRate)
     print("------- TRAINING COMPLETE -------")
 
 def getData():
@@ -87,7 +89,7 @@ def getData():
         # print(i[0])
         top_symptom.append(i[0])
     # print(top_symptom)
-    return(top_symptom)
+    return(top_symptom,symptom_list_filter)
 
 def predict(checked_list):
     # print("This is predicting function")
@@ -95,17 +97,33 @@ def predict(checked_list):
     inputVec = np.zeros(shape=(402,1))
     for item in checked_list:
         # print (item)
-        ind = symptom_list_filter.index(item)
-        # print(ind)
-        inputVec[ind]=1
+        if(item!=""):
+            ind = symptom_list_filter.index(item)
+            # print(ind)
+            inputVec[ind]=1
     # print(inputVec)
     result = nn.feedForward(inputVec)
-    maxVal_pos = result.argmax(axis=0)
-    print(maxVal_pos)
-    print(result[maxVal_pos])
-    predicted_disease = disease_list[int(maxVal_pos)]
-    print(predicted_disease)
-    
+    result_norm = [float(i)/sum(result) for i in result]
+    # result_norm = [float(i)/max(result) for i in result]
+    # print(result_norm)
+
+    # print("\nTop values")
+    top_prediction = nlargest(8, enumerate(result_norm), key=lambda x: x[1])
+    # print(top_prediction,"\n")
+
+    # predicted_values = []
+    predicted_values = [[0 for x in range(8)] for y in range(0)] 
+    for i in top_prediction:
+        tmp = disease_list[i[0]]
+        dis = tmp.split('_', 1)[-1]
+        # print(dis)
+        percent = float(i[1])*100
+        per = "%.5f" % percent + " %"
+        # print(per)
+        # predicted_values.append(dis,per)
+        predicted_values.append([dis,per])
+    # print(predicted_values)
+    return predicted_values
         
 # training()
 # getData()
